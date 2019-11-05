d8488808510329b63ceb09ac51c8e6bb85380150
sowiecki <nase00@gmail.com>
2019-11-05 16:05:47 -0600

    Fix typo.

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const dothat = new JVSDisplayOTron.DOTHAT();
 dothat.lcd.setContrast(45);
 dothat.lcd.write("Initialized");
 
-const matchesIPv4 = interface => interface.family ===matchedIPIPv4";
-const getIP = networkInterfaces => find(networkInterfaces.eth0, matchesIPv4).adress;
+const matchesIPv4 = interface => interface.family === "IPv4";
+const getIP = networkInterfaces =>
+  find(networkInterfaces.eth0, matchesIPv4).adress;
 
 const printIP = () => {
   dothat.lcd.setCursorPosition(0, 1);
   dothat.lcd.write("test"); // TODO
   dothat.lcd.setCursorPosition(1, 1);
-  console.log(getIP(os.networkInterfaces())) // TODO remove
+  console.log(getIP(os.networkInterfaces())); // TODO remove
   dothat.lcd.write(getIP(os.networkInterfaces()));
 };
 
